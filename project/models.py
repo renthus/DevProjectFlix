cb094c1cb6cbee486a078b1b53a349aee305ea69
renthus <renatodasilvamaldonado@gmail.com>
2023-01-13 19:12:27 -0300

    update: created data bases and requirements.txt

--- project/models.py
+++ project/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY_LIST = (
+    ("web_dev","Web Development"),
+    ("cyber","Cyber Security"),
+    ("automation","Automation"),
+)
+
+# Create projects
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    thumb = models.ImageField(upload_to='thumb_projects')
+    description = models.TextField(max_length=2000)
+    category = models.CharField(max_length=20, choices=CATEGORY_LIST)
+    views = models.IntegerField(default=0)
+
+# Create type projects
+
+# Create users

f11d2248df428188b327f9b911dbea6e903e0baa
renthus <renatodasilvamaldonado@gmail.com>
2023-01-19 11:11:08 -0300

    add: views.py - homeprojects

--- project/views.py
+++ project/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, "homepage.html")
+
+def homeprojects(request):
+    return render(request, "homeprojects")
